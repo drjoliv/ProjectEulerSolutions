8619bdc5475bf109b8e08b048c3381248ebaa14c
desonte.jolivet <jolivetdesonte@yahoo.com>
2016-01-06 09:25:22 -0800

    Update

--- p001.hs
+++ p001.hs
@@ -2,7 +2,8 @@
  - Solution to Project Euler problem 1
  - author: Desonte Jolivet
  - Problem: Find the sum of all the multiples of 3 or 5 below 1000.
+ -
  -}
 
-x = sum [x | x <- [1..999], x `mod` 3 ==0 || x `mod` 5 ==0]
-main = print x
+y = sum [x | x <- [1..999], x `mod` 3 ==0 || x `mod` 5 ==0]
+main = print y
