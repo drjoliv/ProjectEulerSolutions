f9e576ba94ca05c186e1aeee8acd4c53c6abfdcf
Desonte Jolivet <jolivetdesonte@yahoo.com>
2016-06-28 21:49:37 -0700

    Better soultion using self referencing list to make infinte list of fib numbers.

--- p002.hs
+++ p002.hs
@@ -7,15 +7,17 @@
  -}
 -- | The fib 'function' returns the nth element in the fibonacci sequence.
 -- The fibonacci sequence is the series of numbers 0, 1, 1, 2, 3, 5, 8, 13, 21, 34
--- The next number in the sequence is found by adding the tqo numbers before it.
--- The Recurance can be defined  a~n~ = (a~n-1~) + (a~n-2~) &#x3BB
+-- The next number in the sequence is found by adding the two numbers before it.
+-- The Recurance can be defined  FIBn = FIB(n-1) + FIB(n-2)
 fib :: Integer -> Integer
 fib x 
-	| x == 1 = 1
-	| x == 2 = 2
-	| x == 3 = 3
-	| otherwise = fib (x - 1) + fib (x - 2)
+    | x == 1 = 0
+    | x == 2 = 1
+    | otherwise = fib (x - 1) + fib (x - 2)
 
---This solution takes much to long i will have to revisit it and determine why
-x = sum (takeWhile (< 4000000) [ans | x <- [1..], let ans = fib x, even ans])
-main = print x
+
+--Infinite list of Fibonacci sequence.
+fibs :: [Integer]
+fibs = 1 : 2 : [ n | x <-[2..], let n = ((fibs !! (x-1)) + (fibs !! (x-2)))]
+better = sum $ takeWhile (<4000000) [x | x <- fibs, even x]
+main = print better
