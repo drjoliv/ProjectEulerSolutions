139bcd4a4917550c38ec674f4b6a673b51819251
Desonte Jolivet <jolivetdesonte@yahoo.com>
2016-06-29 19:13:15 -0700

    Moved fibs and fib to EulerUTIL module.

--- p002.hs
+++ p002.hs
@@ -1,23 +1,15 @@
+import qualified EulerUTIL
+
 {- 
  - Solution to Project Euler problem 2
  - author: Desonte Jolivet
  - Problem: By considering the terms in the Fibonacci sequence whose
  - values do not exceed four million, find the sum of the even-valued terms.
  - 
+ - Description:
+ - [x | x <- EulerUTIL.fibs, even x] list containing even Fibonacci numbers.
+ - takeWhile filters all values above four million.
+ - Then sum adds all those values still in the list.
  -}
--- | The fib 'function' returns the nth element in the fibonacci sequence.
--- The fibonacci sequence is the series of numbers 0, 1, 1, 2, 3, 5, 8, 13, 21, 34
--- The next number in the sequence is found by adding the two numbers before it.
--- The Recurance can be defined  FIBn = FIB(n-1) + FIB(n-2)
-fib :: Integer -> Integer
-fib x 
-    | x == 1 = 0
-    | x == 2 = 1
-    | otherwise = fib (x - 1) + fib (x - 2)
-
-
---Infinite list of Fibonacci sequence.
-fibs :: [Integer]
-fibs = 1 : 2 : [ n | x <-[2..], let n = ((fibs !! (x-1)) + (fibs !! (x-2)))]
-better = sum $ takeWhile (<4000000) [x | x <- fibs, even x]
-main = print better
+ans = sum $ takeWhile (<4000000) [x | x <- EulerUTIL.fibs, even x]
+main = print ans
