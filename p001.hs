1812b23eb541796b54583aecac96219e4d2005e4
Desonte Jolivet <jolivetdesonte@yahoo.com>
2016-07-01 17:43:34 -0700

    Added discreption.

--- p001.hs
+++ p001.hs
@@ -3,7 +3,8 @@
  - author: Desonte Jolivet
  - Multiples of 3 and 5
  - Problem: Find the sum of all the multiples of 3 or 5 below 1000.
+ - Create list comprehension drawing from a list of natural numbers from 1 to 999
+ - Filter the list of 1 to 999 with the predicate x `mod` 3 ==0 || x `mod` 5 ==0
  -}
 
-y = sum [x | x <- [1..999], x `mod` 3 ==0 || x `mod` 5 ==0]
-main = print y
+problem_1 = sum [x | x <- [1..999], x `mod` 3 ==0 || x `mod` 5 ==0]
